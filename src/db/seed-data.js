c1388c3d7eaa2ee278a9b1bee89922019e65f03d
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2025-12-01 14:46:25 -0800

    Fix port to seed MySQL data

--- src/db/seed-data.js
+++ src/db/seed-data.js
@@ -19,7 +19,7 @@ const bcrypt = require('bcryptjs');
 // Database configurations
 const MYSQL_CONFIG = {
   host: process.env.MYSQL_HOST || 'localhost',
-  port: parseInt(process.env.MYSQL_PORT || '3306'),
+  port: parseInt(process.env.MYSQL_PORT || '3307'),
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   database: process.env.MYSQL_DATABASE || 'kayak'
